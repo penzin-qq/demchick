3cfca88d8cc121a0e3c82a9453dfe47b7e4e4591
penzin-qq <nikitapenzin99@gmail.com>
2026-03-24 15:07:54 +0300

    jenkins check test

--- src/test/java/ru/bulgakov/webshop/test/RegistrationTest.java
+++ src/test/java/ru/bulgakov/webshop/test/RegistrationTest.java
@@ -1,17 +1,35 @@
 package ru.bulgakov.webshop.test;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.*;
 import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import ru.bulgakov.webshop.TestBase;
 import ru.bulgakov.webshop.pages.WsWelcomePage;
 
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.SeverityLevel.CRITICAL;
 import static ru.bulgakov.webshop.config.Config.WEB_SHOP_URL;
 
-public class RegistrationTest {
+public class RegistrationTest extends TestBase {
 private static final Faker faker = new Faker();
 
+
+
     @Test
+    @Owner("n.penzin")
+    @Tag("positeve")
+    @Severity(CRITICAL)
+    @Epic("Авторизация")
+    @Feature("Регистрация")
+    @Story("Регистрация нового пользователя")
+    @Link("TASK - 120")
+
+    @DisplayName("Успешная регистрация нового пользователя")
+    @Description("Создаем нового пользователя со случайными данными через интерфейс")
     void registrationTest() {
 
         String password = faker.harryPotter().character() + faker.number().positive();
